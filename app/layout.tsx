fc11dadc6d697130d81036a673254d16f241274d
Ahmed Raza <101836398+devdazzlee@users.noreply.github.com>
2025-07-09 16:38:29 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'AG Sindh Document Managment',
+  description: 'AG Sindh Document Managment',
+  generator: 'Ace Studios',
 }
 
 export default function RootLayout({
